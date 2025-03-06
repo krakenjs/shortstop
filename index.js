a9f512f5616154dab993d72cec37ee8ba03005bc
Gaurav Bharti <dreamsparks@Gauravs-MacBook-Pro.local>
2025-03-06 17:23:19 +0530

    fix(error): made error readable

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ exports.create = function create(parent) {
 
                 fs.readFile(file, 'utf8', function (err, data) {
                     if (err) {
-                        callback(new Error(`Error occured while reading file: ${file}`, {
+                        callback(new Error(`Error occured while reading file ${file}`, {
                             cause: err
                         }));
                         return;
@@ -57,7 +57,7 @@ exports.create = function create(parent) {
                         data = JSON.parse(data);
                         resolve(data, file, callback);
                     } catch (err) {
-                        callback(new Error('Error occured while parsing JSON data', {
+                        callback(new Error(`Error occured while parsing JSON data for file ${file}`, {
                             cause: err
                         }));
                     }
